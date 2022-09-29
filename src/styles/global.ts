83b90e0f2b50953243fe5746ce707bb57bddb543
robsonpereiradasilva <robson.silva@novaandradina.org>
2022-09-29 18:44:14 -0400

    Add global style to the project

---
+++ src/styles/global.ts
@@ -0,0 +1,61 @@
+
+import { createGlobalStyle  } from "styled-components";
+
+export const  GlobalStyle = createGlobalStyle `
+:root {
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --green: #33cc95;
+    --blue: #5429cc;
+    
+    --blue-light: #6933ff;
+    
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #ffffff;
+    --background: #f0f2f5;
+}
+ 
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+}
+
+html{
+    @media(max-with: 1080px){
+        font-size: 93.75%
+    }
+
+    @media(max-width: 72px){
+        font-size: 87.5%
+    }
+} 
+
+body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+}
+
+body, input,textarea,button{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+}
+
+h1,h2,h3,h4,h5,h6,strong{
+    font-weight: 600;
+}
+
+button{
+    cursor: pointer;
+}
+
+[disable]{
+    opacity: 0.6;
+    cursor: not-allowed;
+}
+
+
+
+`
